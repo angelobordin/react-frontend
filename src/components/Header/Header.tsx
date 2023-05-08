7dac8c3aec5d0c03e427d03add9aec5940d81e4e
Angelo Bordin <angelombordin@gmail.com>
2023-05-08 17:18:43 -0300

    fix header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,37 +2,25 @@ import './Header.scss';
 import { BsPersonCircle } from "react-icons/bs";
 
 export default function Header() {
-    return (<> 
+    return (<>
         <header>
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-6">
-                        <div className='company'>
-                            <div className="sigla">
-                                B
-                            </div>
-                            <div className="company-name">
-                                Biovalle Group
-                            </div>
-                        </div>
+            <div className='company'>
+                <div className='first'>
+                    <div className='sigla'>
+                        B
                     </div>
-                    <div className="col-6 p-0">
-                        <div className="second">
-                            <div className="user">
-                                <div className='user-foto'>
-                                    <BsPersonCircle size={32} />
-                                </div>
-                                <div className='user-name'>
-                                    Angelo Bordin
-                                </div>
-                            </div>
-                        </div>
-                        {/* <ul className='lista'>
-                            <li><a href="#">Perfil</a></li>
-                            <li><a href="#">Configurações</a></li>
-                            <li><a href="#">Notificações</a></li>
-                            <li><a href="#">Desconectar</a></li>
-                        </ul> */}
+                    <div className='company-name'>
+                        Biovalle Group
+                    </div>
+                </div>
+            </div>
+            <div className='user'>
+                <div className='second'>
+                    <div className='person'>
+                        <BsPersonCircle size={32} />
+                    </div>
+                    <div className='user-name'>
+                        Angelo Bordin
                     </div>
                 </div>
             </div>
